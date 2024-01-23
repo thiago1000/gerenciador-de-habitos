c6545612ab391acc68717cb3e71f4303b74770fc
Thiago Viana <thiago_rallfall@hotmail.com>
2024-01-23 09:26:45 -0300

    criando o front-end

---
+++ src/app/new-habit/page.tsx
@@ -0,0 +1,34 @@
+const NewHabit = () => {
+  async function newHabit(formData: FormData) {
+    'use server';
+    const habit = formData.get('habit');
+    console.log(habit);
+  }
+
+  return (
+    <main className="container relative flex flex-col gap-8 px-12 pt-16">
+      <h1 className="text-4xl font-light text-white font-display text-center">
+        Novo hábito
+      </h1>
+      <form action={newHabit} className="flex flex-col gap-4 mt-4">
+        <input
+          type="text"
+          name="habit"
+          id="habit"
+          className="p-2 font-sans text-white rounded-md bg-neutral-800"
+        />
+        <button
+          type="submit"
+          className="bg-[#45EDAD] font-display text-neutral-900 font-regular rounded-md text-2xl p-2 mt-8"
+        >
+          Cadastrar
+        </button>
+        <button className="bg-neutral-800 text-[#F85858] font-display font-regular rounded-md text-2xl p-2">
+          Cancelar
+        </button>
+      </form>
+    </main>
+  );
+};
+
+export default NewHabit;
---
+++ src/components/DayState.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+const DayState = ({ day }: { day: boolean | undefined }) => {
+  let image: [string, string, number] = ['images/gray-mark.svg', 'gray', 12];
+
+  if (day === true) image = ['images/check.svg', 'green mark', 24];
+
+  if (day === false) image = ['images/x.svg', 'red x mark', 24];
+
+  const [src, alt, size] = image;
+  return (
+    <div className="flex items-center justify-center h-9">
+      <Image src={src} width={size} height={size} alt={alt} />
+    </div>
+  );
+};
+
+export default DayState;
